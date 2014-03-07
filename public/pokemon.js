28bf8e51bfb6fc4c8fc193d4b5aec97a2eae9723
Renan Greca <renangreca@gmail.com>
2014-03-07 08:47:19 -0800

    Basic NodeJS features

--- public/pokemon.js
+++ public/pokemon.js
@@ -2,7 +2,7 @@ function randomPokemon() {
     var dexno = Math.floor((Math.random()*151)+1);
     $("#whos-this-pokemon").attr(
         'src',
-        "pokemon/"+dexno+".png"
+        "/pokemon/"+dexno+".png"
     );
 }
 
